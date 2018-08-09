fbdfc127dc745ad3d484f6ffc75cc3ec623ab41d
Todd Fine <tfine@Todds-iMac.local>
2018-08-09 13:09:17 -0400

    list and title page

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Title' });
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Express' });
+  res.render('contact', { title: 'Contact' });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Express' });
+  res.render('about', { title: 'About' });
+});
+
+router.get('/all', function(req, res, next) {
+  res.render('alltapes', { title: 'All' });
 });
 
 module.exports = router;
