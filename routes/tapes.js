fdc91d835bb5d766970c4f4f18a949a9fdadb6f7
Todd Fine <tdfine@gmail.com>
2025-12-23 20:35:08 -0600

    fix search

--- routes/tapes.js
+++ routes/tapes.js
@@ -46,6 +46,52 @@ router.get('/', function (req, res) {
       .catch(() => res.render('error'));
   }
 
+  /* Date range search */
+  if (req.query.startDateTime && req.query.endDateTime) {
+    const start = moment(req.query.startDateTime, "MM/DD/YYYY").startOf('day');
+    const end = moment(req.query.endDateTime, "MM/DD/YYYY").endOf('day');
+
+    if (!start.isValid() || !end.isValid()) {
+      return res.render('page', { message: "Please provide a valid date range." });
+    }
+
+    return TapeService.listrange(start.toDate(), end.toDate())
+      .then(tapes => {
+        if (!tapes.length) {
+          return res.render('page', { message: "No results." });
+        }
+        return res.render('shorttapes', {
+          tapes,
+          title: `Date Range: ${req.query.startDateTime} - ${req.query.endDateTime}`
+        });
+      })
+      .catch(() => res.render('error'));
+  }
+
+  /* Specific tape and/or conversation */
+  if (req.query.tapenum || req.query.conversationnum) {
+    if (!req.query.tapenum) {
+      return res.render('page', { message: "Please provide a tape number." });
+    }
+
+    if (req.query.conversationnum) {
+      const code = padzero(req.query.tapenum) + "-" + padzero(req.query.conversationnum);
+      return res.redirect(`/tapes/${code}`);
+    }
+
+    return TapeService.listall(req.query.tapenum)
+      .then(tapes => {
+        if (!tapes.length) {
+          return res.render('page', { message: "No results." });
+        }
+        return res.render('shorttapes', {
+          tapes,
+          title: "Tape " + padzero(req.query.tapenum)
+        });
+      })
+      .catch(() => res.render('error'));
+  }
+
   /* Default listing */
   return TapeService.listall()
     .then(tapes => {
